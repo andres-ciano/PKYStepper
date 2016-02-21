a454a8bc28a202fc80909b1842db6581c4aeba5d
Andres Ciaño <andres.cianio@gmail.com>
2016-02-21 14:14:37 -0300

    Screenshots + english example text

--- PKYStepperExample/ViewController.m
+++ PKYStepperExample/ViewController.m
@@ -34,7 +34,7 @@
     
     // plain
     self.plainStepper = [[PKYStepper alloc] initWithFrame:CGRectMake(x, 100, width, 44)];
-    self.plainStepper.minimumStateString = @"agregar";
+    self.plainStepper.minimumStateString = @"add 1st one";
     [self.plainStepper setLabelTextColor:[UIColor colorWithRed:255.0/255.0 green:98.0/255.0 blue:67.0/255.0 alpha:1.0]];
     [self.plainStepper setLabelFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:15.0f]];
     [self.plainStepper setButtonTextColor:[UIColor colorWithRed:255.0/255 green:98.0/255 blue:67.0/255 alpha:1.0] forState:UIControlStateNormal];
